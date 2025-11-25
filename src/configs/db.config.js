cef1ab94dcbfeefcac409d9db5aa13219fb7ed61
tienmanh240503-cloud <tienmanh240503@gmail.com>
2025-11-25 18:04:41 +0700

    test

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -20,7 +20,7 @@ export const DB_CONFID = {
     },
     mysql_connect: {
         host: process.env.DB_HOST,
-        port: process.env.PORT,
+        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
